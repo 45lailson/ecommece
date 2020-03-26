b8f8ab7f5789ed86ae140518ef66aae7b03afd0d
teste <lailson4545@hotmail.com>
2020-03-26 00:43:18 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 //Diretorios namespace
@@ -7,6 +9,7 @@ require_once("vendor/autoload.php");
 use\Slim\Slim;
 use\Hcode\Page;
 use\Hcode\PageAdmin;
+use\Hcode\Model\User;
 
 //crinado uma nova aplicação
 $app = new Slim();
@@ -25,8 +28,10 @@ $app->get('/', function() {
    
 });
 
-$app->get('/admin', function() {
+$app->get('/admin', function()  {
 
+	User::verifyLogin();
+		
 	//Instancia a classe page
 
 	$page = new PageAdmin();
@@ -34,6 +39,36 @@ $app->get('/admin', function() {
 	$page->setTpl("index");
    
 });
+
+//Rota para acessar o template do login e suas funções
+
+$app->get('/admin/login', function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login' , function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout',function() {
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
+});
+
 
 
 //execulta a função
