114469b67cddb2a3357f4bf0ffbe95a533b0e17f
teste <lailson4545@hotmail.com>
2020-03-30 17:03:39 -0300

    projeto Atualizado

--- index.php
+++ index.php
@@ -28,6 +28,8 @@ $app->get('/', function() {
    
 });
 
+//Criando A Rota para a PageAdmin
+
 $app->get('/admin', function()  {
 
 	User::verifyLogin();
@@ -67,6 +69,153 @@ $app->get('/admin/logout',function() {
 
 	header("Location: /admin/login");
 	exit;
+});
+
+//Listar todos os usuarios
+$app->get("/admin/users", function() {
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users", array(
+
+		"users"=>$users
+
+	));
+
+});
+
+	$app->get("/admin/users/create", function() {
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users-create");
+
+});
+
+	//Rota para excluir usuarios
+
+	$app->get("/admin/users/:iduser/delete", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+
+	exit;
+
+});
+
+	//Rota para atualizar os usuario
+
+	$app->get("/admin/users/:iduser", function($iduser) {
+
+	/*User::verifyLogin();
+
+	$page = new PageAdmin();
+	
+	$page->setTpl("users-update");
+	*/
+
+   User::verifyLogin();
+ 
+   $user = new User();
+ 
+   $user->get((int)$iduser);
+ 
+   $page = new PageAdmin();
+ 
+   $page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+ 
+});
+
+
+$app->post("/admin/users/create", function () {
+
+ 	User::verifyLogin();
+
+	$user = new User();
+
+ 	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+ 	$_POST['despassword'] = password_hash($_POST["despassword"], PASSWORD_DEFAULT, [
+
+ 		"cost"=>12
+
+ 	]);
+
+ 	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+ 	exit;
+
+});
+
+
+$app->post("/admin/users/:iduser", function($iduser) {
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))? 1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+
+	exit;
+
+});
+
+
+$app->get("/admin/forgot", function() {
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+$app->get("/admin/forgot/sent", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+
 });
 
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -8,6 +8,11 @@ use \Hcode\Model;
 class User extends Model {
 
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_Secret";
+	const SECRET_IV = "HcodePhp7_Secret_IV";
+	const ERROR = "UserError";
+	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSucesss";
 
 	public static function login($login, $password)
 	{
@@ -71,6 +76,163 @@ class User extends Model {
 
 	}
 
+
+	//metodo que faz uma lista de usuarios salvos do banco
+
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) ORDER BY b.desperson");
+
+	}
+
+	//Metodo que salva no banco de dados um insert
+
+	public function save()
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin )",
+			array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+			));
+
+
+			$this->setData($results[0]);
+	}
+
+
+	public function get($iduser)
+	{
+ 
+ 		$sql = new Sql();
+ 
+ 		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b USING(idperson) WHERE a.iduser = :iduser;", array(
+ 				":iduser"=>$iduser
+ 			 ));
+ 
+			 $data = $results[0];
+ 
+ 			$this->setData($data);
+ 
+	 }
+
+	 public function update()
+	 {
+
+
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin )",
+			array(
+			"iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+			));
+
+
+			$this->setData($results[0]);
+
+	 }
+
+	 public function delete()
+	 {
+
+	 	$sql = new Sql();
+
+	 	$sql->query("CALL sp_users_delete(:iduser)",array(
+	 		":iduser"=>$this->getiduser()
+	 	));
+	 }
+
+	 public static function getForgot($email) 
+	 {
+	 	$sql = new Sql();
+
+	 	$results = $sql->select("
+	 		SELECT *
+	 		FROM tb_persons a 
+	 		INNER JOIN tb_users b USING(idperson)
+	 		WHERE a.desemail = :email;
+	 	", array(
+	 		":email"=>$email
+	 	));
+
+	 	if(count($results) === 0)
+	 	{
+	 		throw new \Exception("Não foi possivel recupera a senha");
+	 				
+	    }
+	    else
+	    {
+
+	    	$data = $results[0];
+
+	    	$results2 = $sql->select("CALL sp_userspasswordrecoveries_create(:iduser , :desip)",array(
+	    		"iduser"=>$data["iduser"],
+	    		":desip"=>$_SERVER["REMOTE_ADDR"]
+	    	));
+
+	    	if (count($results2) === 0)
+	    	{
+
+	    		throw new \Exception("Não foi possivel recuperar a senha");
+	    		
+	    	}
+	    	else
+	    	{
+
+	    		$dataRecovery = $results2[0];
+
+				$code = openssl_encrypt($dataRecovery['idrecovery'], 'AES-128-CBC', pack("a16", User::SECRET), 0, pack("a16", User::SECRET_IV));
+
+				$code = base64_encode($code);
+
+				if ($inadmin === true) {
+
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
+				} else {
+
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+					
+				}				
+
+				$mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha do Lailson Store", "forgot", array(
+					"name"=>$data['desperson'],
+					"link"=>$link
+				));				
+
+				$mailer->send();
+
+				return $link;
+
+	    	}
+
+
+	    }
+
+
+	
+	}
+
+
+
 }
+
+
 
 ?>
