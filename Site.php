165d5f5d35e6cac52995a791f421e815ae51f7a5
teste <lailson4545@hotmail.com>
2020-04-10 20:56:08 -0300

    Carrinho Produtos

--- Site.php
+++ Site.php
@@ -76,9 +76,70 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
+	$page->setTpl("cart",[
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
+
+	]);
+
 });
 
+//Rota para adicionar um produto no carrinho
+
+$app->get("/cart/:idproduct/add", function($idproduct) {
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i=0; $i <  $qtd ; $i++) { 
+		
+		$cart->addProduct($product);
+
+	}
+
+	header("Location: /cart");
+	exit();
+
+});
+
+//Rota para Excluir um produto no carrinho
+
+$app->get("/cart/:idproduct/minus", function($idproduct) {
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit();
+	
+});
+
+//Rota para Excluir Todos os produto no carrinho
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+
+});
 
 
 
