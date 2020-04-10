b794ef0e6446f92105579b93a08bd068a57ad5d9
teste <lailson4545@hotmail.com>
2020-04-09 23:00:10 -0300

    Detalhes do Produto

--- Site.php
+++ Site.php
@@ -49,6 +49,24 @@ $app->get("/categories/:idcategory", function($idcategory){
 
 });
 
+//Rota para ver detalhes do produto
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail",[
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+
+	]);
+
+});
+
 
 
 
